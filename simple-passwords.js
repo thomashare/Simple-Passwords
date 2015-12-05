01780e92d4c17b105b2c590f566c6628091c1225
Thomas Hare <tommyhares@gmail.com>
2015-12-05 15:37:54 -0700

    Update simple-passwords.js

--- simple-passwords.js
+++ simple-passwords.js
@@ -17,7 +17,7 @@ $('.show-password').attr('title','Hold to show password');
 // show password on mouse down of the eye button
 $('.show-password').mousedown( function() {
   $('.show-password').prev('input').attr('type','text');
-  $('.show-password').parent().prev().last('input').attr('type','text');
+  $('.show-password').parent().prev().prev('input').attr('type','text');
   $('.show-password').removeClass('glyphicon-eye-close');
   $('.show-password').addClass('glyphicon-eye-open');
 });
@@ -25,7 +25,7 @@ $('.show-password').mousedown( function() {
 // hide password of mouse up of the eye button
 $('.show-password').mouseup( function() {
   $('.show-password').prev('input').attr('type','password');
-  $('.show-password').parent().prev().last('input').attr('type','password');
+  $('.show-password').parent().prev().prev('input').attr('type','password');
   $('.show-password').addClass('glyphicon-eye-close');
   $('.show-password').removeClass('glyphicon-eye-open');
 });
@@ -33,7 +33,7 @@ $('.show-password').mouseup( function() {
 // hide password of mouse out of the eye button
 $('.show-password').mouseleave( function() {
   $('.show-password').prev('input').attr('type','password');
-  $('.show-password').parent().prev().last('input').attr('type','password');
+  $('.show-password').parent().prev().prev('input').attr('type','password');
   $('.show-password').addClass('glyphicon-eye-close');
   $('.show-password').removeClass('glyphicon-eye-open');
 });
